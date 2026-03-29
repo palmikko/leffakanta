a1fb2cc820ad8d3635dc01a9453c461216031c77
palmikko <267936079+palmikko@users.noreply.github.com>
2026-03-29 23:03:19 +0300

    Add new movie form

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_movie")
+def new_movie():
+    return render_template("new_movie.html")
+
+@app.route("/create_movie", methods=["GET", "POST"])
+def create_movie():
+    title = request.form["title"]
+    genre = request.form["genre"]
+    duration = request.form["duration"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO movies (title, genre, duration, user_id)
+             VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, genre, duration, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -42,10 +59,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/login")
         else:
@@ -53,5 +73,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/login")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY,
+    title TEXT UNIQUE,
+    genre TEXT,
+    duration INTEGER,
+    user_id INTEGER REFERENCES users
+);
